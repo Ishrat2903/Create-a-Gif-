9e55780b9cbc36f0782a7cedb08ccd35921d47e1
Ishrat Shaikh <132141246+Ishrat2903@users.noreply.github.com>
2026-04-15 12:28:13 +0200

    Add files via upload

---
+++ proj_gif.py
@@ -0,0 +1,14 @@
+import imageio.v3 as iio
+import os
+
+print("Saving to:", os.getcwd())
+
+filenames = ['team-pic1.png', 'team-pic2.png']
+images = [ ]
+
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('team.gif', images, duration = 500, loop= 0)
+
+print("GIF created successfully!")
